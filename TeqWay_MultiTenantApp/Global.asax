81bbe25afca33e709ab99b2a4c103adedc05dc20
sazhar <saqibabdullahazhar>
2018-09-24 20:48:11 +0500

    first commit

---
+++ TeqWay_MultiTenantApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleLogix_MultiTenantApp.MvcApplication" Language="C#" %>
---
+++ TeqWay_MultiTenantApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleLogix_MultiTenantApp.Startup))]
+namespace SimpleLogix_MultiTenantApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
